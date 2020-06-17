24ce3956e407738af1c784baf155386ff0d3eb72
zidz <zidios101@gmail.com>
2020-06-18 01:02:38 +0800

    update ui and function

--- dist/escooter-dashboard.e31bb0bc.js
+++ dist/escooter-dashboard.e31bb0bc.js
@@ -53659,7 +53659,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54226" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55050" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
